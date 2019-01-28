ede72f27b8dc35a1cebd4b42616d4432346d26c7
Jorge Luis Rojas Arcos <jorgelroar@gmail.com>
2019-01-27 23:01:59 -0600

    Initial commit

---
+++ config.php
@@ -0,0 +1,33 @@
+<?php
+	date_default_timezone_set('America/Mexico_City');
+	error_reporting(E_ALL);
+	ini_set('display_errors', 'on');
+
+	define( 'URL' ,"http://localhost/".basename(getcwd())."/");
+	define( 'CSS' ,URL."public/css/");
+	define( 'JS' , URL."public/js/" );
+	define( 'IMG', URL."public/img/");
+	define( 'LIB', URL."libs/");
+
+	//Crean el archivo de config.js
+	//Comentar al primer uso, no es necesario sobreescribir
+	@$file = fopen("public/js/config.js", "w");
+	@fwrite($file,
+		'var config = {
+			url: "'.URL.'",
+			img: "'.URL.'public/images/"
+		}');
+	@fclose($file);
+
+	//Constantes de la base de datos
+	define( 'DB_HOST' , 'localhost');
+	define( 'DB_USER' , 'root');
+	define( 'DB_PASS' , '');
+	define( 'DB_NAME' , '');
+
+	define('DB_CHARSET', 'utf-8');
+	
+
+	define( 'ALGOR', 'md5');
+	define( 'KEY', 'oPMh543YGvb==?');
+?>
